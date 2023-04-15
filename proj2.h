8fe3f3909d5aa1e43232f93336d9e9f00a33659e
Michal Balogh <baloghmichal03@gmail.com>
2023-04-16 00:25:10 +0200

    Argument parser

--- proj2.h
+++ proj2.h
@@ -11,3 +11,89 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <limits.h>
+
+// sem_init(&sem, 0, 1);
+// sem_wait(&sem);
+// sem_post(&sem);
+// sem_destroy(&sem); 
+
+// ./proj2 NZ NU TZ TU F
+
+
+/**
+ * @brief Parse arguments from command line
+ * 
+ * @param argc Number of arguments
+ * @param argv Array of arguments
+ * @param NZ Number of customers
+ * @param NU Number of officials
+ * @param TZ Maximum time in milliseconds that a customer waits after creating their ticket before entering the post office (or leaving without being served). 0<=TZ<=10000
+ * @param TU Maximum break time in milliseconds for officials. 0<=TU<=100
+ * @param F Maximum time in milliseconds that the post office is closed for new customers. 0<=F<=10000
+ * 
+ * @return int 0 if success, 1 if error
+ */
+int parse_args(int argc, char *argv[], int *NZ, int *NU, int *TZ, int *TU, int *F);
+
+/**
+ * @brief Validate argument
+ * 
+ * @param arg Argument to validate
+ * 
+ * @return int -1 if error, argument value if success
+ */
+int validate_arg(char *arg);
+
+
+//====================================================================================================
+
+
+int parse_args(int argc, char *argv[], int *NZ, int *NU, int *TZ, int *TU, int *F)
+{
+    const int expected_num_args = 5;
+
+    if (argc != expected_num_args + 1)
+    {
+        fprintf(stderr, "Error: wrong number of arguments.\n");
+        return 1;
+    }
+
+    // Array of pointers that stores values of arguments.
+    int *arg_values[] = {NZ, NU, TZ, TU, F};
+    // Constraints for size of arguments TZ, TU, F.
+    const int max_values[] = {INT_MAX, INT_MAX, 10000, 100, 10000};
+    // Names of arguments for error message.
+    const char *arg_names[] = {"NZ", "NU", "TZ", "TU", "F"};
+
+    // Validate arguments.
+    for (int i = 0; i < expected_num_args; i++)
+    {
+        int arg_value = validate_arg(argv[i + 1]);
+        
+        if (arg_value < 0 || arg_value > max_values[i])
+        {
+            fprintf(stderr, "Error: invalid argument %s.\n", arg_names[i]);
+            return 1;
+        }
+        // Store argument value.
+        *(arg_values[i]) = arg_value;
+    }
+
+    return 0;
+}
+
+int validate_arg(char *arg)
+{
+    char *endptr;
+    const long arg_value = strtol(arg, &endptr, 10);
+    // Check if argument was successfully converted to long.
+    if (endptr == arg || arg_value > INT_MAX) {
+        return -1;
+    }
+
+    return arg_value;
+}
+
---
+++ test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+#./proj2 NZ NU TZ TU F
+
+testsVal=(
+    "1 1 1 1 1"
+    "100 010 123 100 123"
+    "100000000 1000 100 100 1"
+)
+
+
+testsInv=(
+    ""
+    "a 1 1 1 1"
+    "1 a 1 1 1"
+    "1 1 a 1 1"
+    "1 1 1 a 1"
+    "1 1 1 1 a"
+    "1 1 1 1 1 a"
+    "-1 1 1 1 1"
+    "1 1 1 1 -1"
+    "100 10 10001 100 10000"
+    "100 10 10000 101 10000"
+    "100 10 10000 100 10001"
+)
+
+echo "Valid aruments:"
+for i in "${!testsVal[@]}"; do
+    echo "Test $((i+1)):"
+    ./proj2 ${testsVal[$i]}
+    if [ $? -eq 0 ]; then
+        echo "OK✅"
+    else
+        echo "FAIL❌"
+    fi
+    echo ""
+done
+
+echo "Invalid aruments:"
+for i in "${!testsInv[@]}"; do
+    echo "Test $((i+1)):"
+    ./proj2 ${testsInv[$i]}
+    if [ $? -eq 1 ]; then
+        echo "OK✅"
+    else
+        echo "FAIL❌"
+    fi
+    echo ""
+done
